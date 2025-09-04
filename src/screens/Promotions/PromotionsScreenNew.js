a1fd36971152f762eb939c2e2a28bdc8121e4c24
clarkybrian <enablermoney@gmail.com>
2025-09-04 03:53:10 +0200

    feat(promotions): ajouter un nouvel écran de promotions et une configuration de logs de débogage

---
+++ src/screens/Promotions/PromotionsScreenNew.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ElegantPromotionsScreen from './ElegantPromotionsScreen';
+
+// Utiliser le nouvel écran élégant par défaut
+const PromotionsScreen = () => {
+  return <ElegantPromotionsScreen />;
+};
+
+export default PromotionsScreen;
